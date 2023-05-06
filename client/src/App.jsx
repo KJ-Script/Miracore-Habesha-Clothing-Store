b2fbe69918f58f10ddfb21501079c95f8d70ab4a
Keti Yohannes <ketiyohannes@gmail.com>
2023-05-06 10:25:24 +0000

    fixed routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import NavBar from "./components/NavBar";
 
 function App() {
 
   return (
     <div className="font-outfit">
       <Router>
+        <NavBar />
         <Routes>
           <Route exact path="/" element={<LandingPage />} />
         </Routes>
