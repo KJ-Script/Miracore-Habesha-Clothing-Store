93da1cd5cc73177958455c3e97589745434dd8a4
Keti Yohannes <ketiyohannes@gmail.com>
2023-05-07 06:26:28 +0000

    started shopping details page

--- client/src/api/menadditional.js
+++ client/src/api/menadditional.js
@@ -1,34 +1,40 @@
-import b1 from '../assets/images/additionals/mennecklace.png'
-import b2 from '../assets/images/additionals/menbracelet.png'
-import b3 from '../assets/images/additionals/menbracelet2.png'
-import b4 from '../assets/images/additionals/menbracelet3.png'
-
+import b1 from "../assets/images/additionals/mennecklace.png";
+import b2 from "../assets/images/additionals/menbracelet.png";
+import b3 from "../assets/images/additionals/menbracelet2.png";
+import b4 from "../assets/images/additionals/menbracelet3.png";
 
 const menAdditional = [
-    {
-        category: 'additional',
-        description: 'Wonderful Traditional Necklace for Men',
-        img: b1,
-    },
+  {
+    category: "additional",
+    description: "Wonderful Traditional Necklace for Men",
+    img: b1,
+    img1: b2,
+    img2: b3,
+  },
 
-    {
-        category: 'additional',
-        description: 'Eccentric yet tasteful bracelet',
-        img: b2,
-    },
+  {
+    category: "additional",
+    description: "Eccentric yet tasteful bracelet",
+    img: b2,
+    img1: b1,
+    img2: b3,
+  },
 
-    {
-        category: 'additional',
-        description: 'Elegant and Minimal',
-        img: b3,
-    },
+  {
+    category: "additional",
+    description: "Elegant and Minimal",
+    img: b3,
+    img1: b2,
+    img2: b1,
+  },
 
+  {
+    category: "additional",
+    description: "Manly yet fashionable",
+    img: b4,
+    img1: b2,
+    img2: b3,
+  },
+];
 
-    {
-        category: 'additional',
-        description: 'Manly yet fashionable',
-        img: b4,
-    },
-]
-
-export default menAdditional
+export default menAdditional;
--- client/src/pages/ShoppingDetails.jsx
+++ client/src/pages/ShoppingDetails.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 
 function ShoppingDetails() {
     const location =  useLocation()
-
+    const [selected, setSelected] = useState('md')
     const item = location.state?.item
-    console.log("item Data: ", item)
+
   return (
-    <div>ShoppingDetails</div>
+    <div>
+      <div className="flex my-12 w-full justify-center">
+        <div className="w-[70%] justify-between">
+        <div className='flex justify-center'>        <div className="hover:p-12 hover:duration-500 mx-2">
+          <img alt={'itempicture2'} src={item.img} className='w-[600px] h-[500px] border border-black p-6'/>
+        </div>
+        <div className="">
+          <img alt={'itempicture2'} src={item.img1} className='w-[300px] h-[250px] border border-black p-6 my-2 hover:p-2 hover:duration-500'/>
+          <img alt={'itempicture2'} src={item.img2} className='w-[300px] h-[250px] border border-black p-6 hover:p-2 hover:duration-500'/>
+        </div>
+        </div>
+        </div>
+
+        <div>
+          <div>Here is some arbitrary Information about the Item</div>
+          <div className="flex justify-between w-full">
+          <div className={selected == 'xs' ? `bg-black text-white border border-black p-2` : `border border-black p-2`} onClick={() => {
+            setSelected('xs')
+          }}>Xs</div>
+          <div className={selected == 'sm' ? `bg-black text-white border border-black p-2` : `border border-black p-2`} onClick={() => {
+            setSelected('sm')
+          }}>Sm</div>
+          <div className={selected == 'md' ? `bg-black text-white border border-black p-2` : `border border-black p-2`} onClick={() => {
+            setSelected('md')
+          }}>Md</div>
+          <div className={selected == 'lg' ? `bg-black text-white border border-black p-2` : `border border-black p-2`} onClick={() => {
+            setSelected('lg')
+          }}>Lg</div>
+          <div className={selected == 'xl' ? `bg-black text-white border border-black p-2` : `border border-black p-2`} onClick={() => {
+            setSelected('xl')
+          }}>Xl</div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
